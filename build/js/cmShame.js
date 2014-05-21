4a14a368c2d2877d81d6ad4a3a4b36a92bf15cda
connormcguire <connormcguire@MBP.local>
2014-05-21 11:27:40 -0700

    V1 HTML For Review
    As far as I can tell, none of this isn’t horribly broken.

---
+++ build/js/cmShame.js
@@ -0,0 +1,44 @@
+
+			$(function(){	
+				var aside = $('.aside');
+				var asideAlt = $('.asideAlt');
+				var divWidth = aside.width(); 
+				var hero = $('.hero');
+				var heroText = $('.heroText');
+				var vertAlign = $('.vertAlign');
+				var asideHover = $('.asideHover');
+				var besideAside = $('.besideAside');
+
+				
+			   aside.height(divWidth);
+			   asideAlt.height(divWidth);
+			
+			   besideAside.height(divWidth);
+			   besideAside.css("display","table");
+			   asideAlt.find('h4').css("margin-top", (asideAlt.height()/2) + 'px');
+			   aside.find('h4').css("margin-top", (aside.height()/2-35) + 'px');
+			   heroText.css("top", (hero.height()/2));
+
+	
+			$(window).resize(function(){
+				var aside = $('.aside');
+				var asideAlt = $('.asideAlt');
+				var divWidth = aside.width(); 
+				var hero = $('.hero');
+				var heroText = $('.heroText');
+				var vertAlign = $('.vertAlign');
+				var asideHover = $('.asideHover');
+				var besideAside = $('.besideAside');
+				
+			   aside.height(divWidth);
+			   asideAlt.height(divWidth);
+			
+			   besideAside.height(divWidth);
+			   besideAside.css("display","table");
+			   asideAlt.find('h4').css("margin-top", (asideAlt.height()/2) + 'px');
+			   aside.find('h4').css("margin-top", (aside.height()/2-35) + 'px');
+			   heroText.css("top", (hero.height()/2));9
+ 
+			});
+		});
+			
