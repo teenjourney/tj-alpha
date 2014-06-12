95b8f4826654d980e8181ae735779548dd64fbdc
connormcguire <connormcguire@mbp.vn.shawcable.net>
2014-06-12 12:10:57 -0700

    updated javascript
    added more shame.

--- build/js/cmShame.js
+++ build/js/cmShame.js
@@ -1,47 +1,53 @@
-
-			$(function(){	
-				var aside = $('.aside');
-				var asideAlt = $('.asideAlt');
-				var divWidth = aside.width(); 
-				var hero = $('.hero');
-				var heroText = $('.heroText');
-				var vertAlign = $('.vertAlign');
-				var asideHover = $('.asideHover');
-				var besideAside = $('.besideAside');
-
-				$('.textGrid').find(".four").css("width","33.3%");
-				$('.threeGrid').find(".four").css("width","33.3%");
-			  
-			   aside.height(divWidth);
-			   asideAlt.height(divWidth);
-			
-			   besideAside.height(divWidth);
-			   besideAside.css("display","table");
-			   asideAlt.find('h4').css("margin-top", (asideAlt.height()/2) + 'px');
-			   aside.find('h4').css("margin-top", (aside.height()/2-35) + 'px');
-			   
-			   
-
-
+$(function() {
+	var aside = $('.aside');
+	var asideAlt = $('.asideAlt');
+	var divWidth = aside.width();
+	var besideAside = $('.besideAside');
+	$('.textGrid').find(".four").css("width", "33.3%");
+	$('.threeGrid').find(".four").css("width", "33.3%");
+	aside.height(divWidth);
+	asideAlt.height(divWidth);
+	besideAside.height(divWidth);
+	besideAside.css("display", "table");
+	asideAlt.find('h4').css("margin-top", (asideAlt.height() / 2) + 'px');
+	aside.find('h4').css("margin-top", (aside.height() / 2 - 35) + 'px');
 	
-			$(window).resize(function(){
-				var aside = $('.aside');
-				var asideAlt = $('.asideAlt');
-				var divWidth = aside.width(); 
-				var hero = $('.hero');
-				var heroText = $('.heroText');
-				var vertAlign = $('.vertAlign');
-				var asideHover = $('.asideHover');
-				var besideAside = $('.besideAside');
-				
-			   aside.height(divWidth);
-			   asideAlt.height(divWidth);
-			
-			   besideAside.height(divWidth);
-			   besideAside.css("display","table");
-			   asideAlt.find('h4').css("margin-top", (asideAlt.height()/2) + 'px');
-			   aside.find('h4').css("margin-top", (aside.height()/2-35) + 'px');
- 
-			});
-		});
-			
+	var pSize = $('p').css('font-size');
+	var pLine = $('p').css('line-height');
+	var btnFont = $('readMore a').css('font-size');
+	var btnLine = $('readMore a').css('line-height');
+
+	if ($(window).width() < 760) {
+	$('.nine').addClass('eleven').removeClass('nine');
+	$('p').css('font-size', '1.7em').css('line-height','1.6em');
+	$('orangeBtn readMore a').css('font-size','2em').css('line-height','1.6em');
+	}else {
+			$('p').css('font-size', pSize).css('line-height',pLine);
+			$('orangeBtn readMore a').css('font-size', btnFont);
+			}
+	
+	$(window).resize(function() {
+		var aside = $('.aside');
+		var asideAlt = $('.asideAlt');
+		var divWidth = aside.width();
+		var besideAside = $('.besideAside');
+		aside.height(divWidth);
+		asideAlt.height(divWidth);
+		besideAside.height(divWidth);
+		besideAside.css("display", "table");
+		asideAlt.find('h4').css("margin-top", (asideAlt.height() / 2) + 'px');
+		aside.find('h4').css("margin-top", (aside.height() / 2 - 35) + 'px');
+		
+		
+		
+		
+		if ($(window).width() < 760) {
+		$('.nine').addClass('eleven').removeClass('nine');
+		$('p').css('font-size', '1.7em').css('line-height','1.6em');
+		$('orangeBtn readMore a').css('font-size','2em').css('line-height','1.6em');
+		}else {
+			$('p').css('font-size', pSize).css('line-height',pLine);
+			$('orangeBtn readMore a').css('font-size', btnFont).css('line-height',btnLine);
+			}
+	});
+});
